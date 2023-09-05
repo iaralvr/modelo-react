b60400f6ca4fe1102610b76e72d68bd4159fbbeb
iaralvr <iaralanzasenai@gmail.com>
2023-09-05 16:25:19 -0300

    login pronto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Avatar, Button } from "@mui/material";
+
 function App(props) {
  return (
     <>
+    <h1>Home</h1>
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+    <Avatar alt="Iara Lanza" src="/static/images/avatar/1.jpg" />
+    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+  
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./App";
+import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './Login';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>
+  },
+  {
+    path: "/login",
+    element:<Login/>
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App nota="10" />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
+
 );
